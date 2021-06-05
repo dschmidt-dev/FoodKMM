0162d8d263dc95a91c502cbb25de22c8858d6b68
Daniel Schmidt <dschmidt1985@gmail.com>
2021-06-05 19:32:59 +0200

    first view models and views injected by hilt

--- androidApp/src/main/java/dev/dschmidt/foodkmm/android/presentation/navigation/Navigation.kt
+++ androidApp/src/main/java/dev/dschmidt/foodkmm/android/presentation/navigation/Navigation.kt
@@ -5,28 +5,41 @@ import androidx.compose.material.Button
 import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.currentComposer
+import androidx.compose.ui.platform.LocalContext
+import androidx.hilt.navigation.HiltViewModelFactory
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.navArgument
 import androidx.navigation.compose.rememberNavController
 import dev.dschmidt.foodkmm.Greeting
+import dev.dschmidt.foodkmm.android.presentation.recipe_detail.RecipeDetailScreen
+import dev.dschmidt.foodkmm.android.presentation.recipe_detail.RecipeDetailViewModel
+import dev.dschmidt.foodkmm.android.presentation.recipe_list.RecipeListScreen
+import dev.dschmidt.foodkmm.android.presentation.recipe_list.RecipeListViewModel
 
 @Composable
 fun Navigation() {
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = Screen.RecipeList.route) {
         composable(route = Screen.RecipeList.route) { navBackStackEntry ->
-            Column {
-                Text(Greeting().greeting())
-                Divider()
-                Button( onClick = {navController.navigate(Screen.RecipeDetail.route)}) {
-                    Text("Go To Detail")
-                }
+            val factory = HiltViewModelFactory(LocalContext.current, navBackStackEntry = navBackStackEntry)
+            val viewModel: RecipeListViewModel = viewModel("RecipeListViewModel", factory)
+            RecipeListScreen { recipeId ->
+                navController.navigate(Screen.RecipeDetail.route + "/$recipeId")
             }
         }
-        composable(route = Screen.RecipeDetail.route) { navBackStackEntry ->
-            Column {
-                Text("DETAIL ")
-            }
+        composable(
+            route = Screen.RecipeDetail.route + "/{recipeId}",
+            arguments = listOf(navArgument("recipeId") {
+                type = NavType.IntType
+            })
+        ) { navBackStackEntry ->
+            val factory = HiltViewModelFactory(LocalContext.current, navBackStackEntry = navBackStackEntry)
+            val viewModel: RecipeDetailViewModel = viewModel("RecipeDetailViewModel", factory)
+            RecipeDetailScreen(recipeId = viewModel.recipeId.value)
         }
     }
 }
---
+++ androidApp/src/main/java/dev/dschmidt/foodkmm/android/presentation/recipe_detail/RecipeDetailScreen.kt
@@ -0,0 +1,20 @@
+package dev.dschmidt.foodkmm.android.presentation.recipe_detail
+
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun RecipeDetailScreen(
+    recipeId: Int?,
+){
+    if(recipeId == null){
+        Text("Unable to get the details of this recipe...")
+    }
+    else{
+        Column{
+            Text("RecipeDetailScreen: $recipeId")
+        }
+    }
+}
